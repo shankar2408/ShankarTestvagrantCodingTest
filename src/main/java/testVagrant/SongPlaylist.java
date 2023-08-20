7086e6180fca92c17241dd1fd000781f504fbf0d
shankar2408 <shankarec24@gmail.com>
2023-08-20 22:28:49 +0530

    coding test pushed

--- src/main/java/testVagrant/SongPlaylist.java
+++ src/main/java/testVagrant/SongPlaylist.java
@@ -2,8 +2,6 @@ package testVagrant;
 
 import java.util.*;
 
-
-
 class SongPlaylist {
 	private final int initialCapacity;
 	private Map<String, LinkedList<String>> songStore;
@@ -16,21 +14,20 @@ class SongPlaylist {
 	public void addSong(String user, String song) {
 		songStore.putIfAbsent(user, new LinkedList<String>());
 		LinkedList<String> playlist = songStore.get(user);
-		
-        // Remove the song if it already exists
+
+		// Remove the song if it already exists
 		playlist.remove(song);
 
 		if (playlist.size() >= initialCapacity) {
 			// Remove the least recently played song
-			playlist.removeFirst(); 
+			playlist.removeFirst();
 		}
 		// Add the new song to the playlist
-		playlist.add(song); 
+		playlist.add(song);
 	}
 
 	public List<String> getRecentlyPlayed(String user) {
 		return songStore.getOrDefault(user, new LinkedList<String>());
 	}
-
 
 }
--- src/main/java/testVagrant/SongplaylistTest.java
+++ src/main/java/testVagrant/SongplaylistTest.java
@@ -7,8 +7,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SongplaylistTest {
-	
-	//tested with given testcase
+
+	// tested with given testcase
 	@Test
 	public void test1() {
 		SongPlaylist store = new SongPlaylist(3);
@@ -16,66 +16,65 @@ public class SongplaylistTest {
 		store.addSong("user1", "S1");
 		store.addSong("user1", "S2");
 		store.addSong("user1", "S3");
-		//When S4 Song is played
+		// When S4 Song is played
 		store.addSong("user1", "S4");
-		//When S2 Song is played
+		// When S2 Song is played
 		store.addSong("user1", "S2");
-		//When S1 Song is played
+		// When S1 Song is played
 		store.addSong("user1", "S1");
-
 
 		// Output: [S4, S2, S1]
 		List<String> actual = store.getRecentlyPlayed("user1");
-        List<String> expected = Arrays.asList("S4", "S2", "S1");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("S4", "S2", "S1");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	//tested with 2 songs
+
+	// tested with 2 songs
 	@Test
 	public void test2() {
 		SongPlaylist store = new SongPlaylist(3);
 
 		store.addSong("user2", "S1");
 		store.addSong("user2", "S2");
-		
+
 		List<String> actual = store.getRecentlyPlayed("user2");
-        List<String> expected = Arrays.asList("S1", "S2");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("S1", "S2");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	//tested with 1 song
+
+	// tested with 1 song
 	@Test
 	public void test3() {
 		SongPlaylist store = new SongPlaylist(3);
 
 		store.addSong("user3", "S1");
-		
+
 		List<String> actual = store.getRecentlyPlayed("user3");
-        List<String> expected = Arrays.asList("S1");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("S1");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	//tested with empty song
+
+	// tested with empty song
 	@Test
 	public void test4() {
 		SongPlaylist store = new SongPlaylist(3);
 
 		store.addSong("user4", "");
-		
+
 		List<String> actual = store.getRecentlyPlayed("user4");
-        List<String> expected = Arrays.asList("");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	//tested with 3 songs
+
+	// tested with 3 songs
 	@Test
 	public void test5() {
 		SongPlaylist store = new SongPlaylist(3);
@@ -83,16 +82,15 @@ public class SongplaylistTest {
 		store.addSong("user5", "S1");
 		store.addSong("user5", "S2");
 		store.addSong("user5", "S3");
-		
+
 		List<String> actual = store.getRecentlyPlayed("user5");
-        List<String> expected = Arrays.asList("S1", "S2", "S3");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("S1", "S2", "S3");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	
-	//tested with same song again
+
+	// tested with same song again
 	@Test
 	public void test6() {
 		SongPlaylist store = new SongPlaylist(3);
@@ -101,14 +99,11 @@ public class SongplaylistTest {
 		store.addSong("user6", "S2");
 		store.addSong("user6", "S1");
 
-		
 		List<String> actual = store.getRecentlyPlayed("user6");
-        List<String> expected = Arrays.asList("S2", "S1");
-        System.out.println("actual:"+actual);
-        System.out.println("exp:"+expected);
-        Assert.assertEquals(actual, expected);
+		List<String> expected = Arrays.asList("S2", "S1");
+		System.out.println("actual:" + actual);
+		System.out.println("exp:" + expected);
+		Assert.assertEquals(actual, expected);
 	}
-	
-	
 
 }
